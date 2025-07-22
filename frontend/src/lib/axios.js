b16452456c3eb5f5ee7dbba289e886ac777f1940
DanyaLizanetc <a72908383@gmail.com>
2025-07-22 16:32:22 +0300

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5001";
 
 export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
-  withCredentials: true, // send cookies with the request
+  baseURL: `${BASE_URL}/api`, 
+  withCredentials: true,
 });
