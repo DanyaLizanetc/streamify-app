10b6e8d4a8ef5c27d8003128da68a1d18d7017e5
DanyaLizanetc <a72908383@gmail.com>
2025-07-23 18:08:39 +0300

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -51,11 +51,12 @@ export async function signup(req, res) {
     });
 
     res.cookie("jwt", token, {
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      httpOnly: true,
-      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
-      secure: process.env.NODE_ENV === "production",
-    });
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+  secure: process.env.NODE_ENV === "production",
+  domain: process.env.NODE_ENV === "production" ? ".onrender.com" : "localhost", // АБО ТВОЯ АКТУАЛЬНА URL
+});
 
     res.status(201).json({ success: true, user: newUser });
   } catch (error) {
@@ -83,11 +84,12 @@ export async function login(req, res) {
     });
 
     res.cookie("jwt", token, {
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      httpOnly: true,
-      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
-      secure: process.env.NODE_ENV === "production",
-    });
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+  secure: process.env.NODE_ENV === "production",
+  domain: process.env.NODE_ENV === "production" ? ".onrender.com" : "localhost", // АБО ТВОЯ АКТУАЛЬНА URL
+});
 
     res.status(200).json({ success: true, user });
   } catch (error) {
